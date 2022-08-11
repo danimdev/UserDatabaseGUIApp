a1d9686f204b894b404995caed55487c22aea56b
danimdev <106762674+danimdev@users.noreply.github.com>
2022-08-11 18:15:35 +0200

    Show data from selecteditem in the textboxes

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,26 +12,42 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using UserDatabaseGUIApp.Models;
 
 namespace UserDatabaseGUIApp
 {
     public partial class MainWindow : Window
     {
-        
-
         public MainWindow()
         {
             InitializeComponent();
             GiveDataToGrid();
-
         }
 
         public void GiveDataToGrid()
         {
-            using(var context = new UserDB())
+            using (var context = new UserDB())
             {
                 var query = from user in context.Users select user;
-                UserDataGrid.ItemsSource = query.ToList() ;
+                UserDataGrid.ItemsSource = query.ToList();
+            }
+        }
+
+        private void UserDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                User user = (User)UserDataGrid.SelectedItem;
+
+                IDTextBox.Text = user.ID.ToString();
+                UsernameTextBox.Text = user.Username;
+                PasswordTextBox.Text = user.Password;
+            }
+            catch (Exception ex)
+            {
+                IDTextBox.Text = "";
+                UsernameTextBox.Text = "";
+                PasswordTextBox.Text = "";
             }
         }
     }
